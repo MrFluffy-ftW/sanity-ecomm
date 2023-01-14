20be89292a9da9b396bc2e36a88a7cf80d59e015
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2023-01-14 18:55:06 +0545

    Product

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2023 BuyMe &copy; All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>ShopMe</title>
+        <title>BuyMe</title>
       </Head>
       <header>
         <Navbar />
